9f643643c77ebc101ed16ca81a7e5ef6c0cf5eb0
dmitry-sharashkin <faustfbs@mail.ru>
2023-08-13 13:42:46 +0300

    fix currency decimals

--- src/utils/transformCurrencyValue.ts
+++ src/utils/transformCurrencyValue.ts
@@ -20,21 +20,29 @@ function currencySignSwitcher(currency: string) {
 }
 
 export function transformCurrencyValue(number: number, settings?: { currency: 'USDT' | 'RUB' | 'usdt' | 'rub' }) {
-  const shortNumber = number.toFixed(2);
-  const [leftVal, rightVal] = shortNumber.split('.');
-  const reversedValArray = leftVal.split('').reverse();
-  let validLeftVal = reversedValArray
-    .map((i, idx) =>
-      (idx + 1) % 3 === 0 && (idx !== reversedValArray.length || (reversedValArray.includes('-') && idx !== reversedValArray.length - 1))
-        ? ` ${i}`
-        : i
-    )
-    .reverse()
-    .join('');
-  if (validLeftVal.split('')[0] === ' ') {
-    validLeftVal = validLeftVal.replace(' ', '');
-  }
-  let value = `${validLeftVal}${+rightVal > 0 ? `,${+rightVal}` : ''}`;
+  // const shortNumber = number.toFixed(2);
+  // const [leftVal, rightVal] = shortNumber.split('.');
+  // const reversedValArray = leftVal.split('').reverse();
+  // let validLeftVal = reversedValArray
+  //   .map((i, idx) =>
+  //     (idx + 1) % 3 === 0 && (idx !== reversedValArray.length || (reversedValArray.includes('-') && idx !== reversedValArray.length - 1))
+  //       ? ` ${i}`
+  //       : i
+  //   )
+  //   .reverse()
+  //   .join('');
+  // if (validLeftVal.split('')[0] === ' ') {
+  //   validLeftVal = validLeftVal.replace(' ', '');
+  // }
+  // let value = `${validLeftVal}${+rightVal > 0 ? `,${+rightVal}` : ''}`;
+  // if (settings?.currency) {
+  //   value = `${value} ${currencySignSwitcher(settings?.currency?.toLowerCase())}`;
+  // }
+  // return value;
+  const formatter = new Intl.NumberFormat(undefined, {
+    maximumFractionDigits: 2
+  });
+  let value = formatter.format(number);
   if (settings?.currency) {
     value = `${value} ${currencySignSwitcher(settings?.currency?.toLowerCase())}`;
   }
